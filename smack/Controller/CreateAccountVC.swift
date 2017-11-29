74d70100da580083a6f389f8c2f069fbe2f195eb
Alex LaDue <aj.ladue@gmail.com>
2017-11-28 21:37:17 -0500

    Lesson 5 - Create CreateAccount VC and segue back to the channelsVC instead of the login screen

--- smack/Controller/CreateAccountVC.swift
+++ smack/Controller/CreateAccountVC.swift
@@ -1,5 +1,5 @@
 //
-//  LoginVC.swift
+//  CreateAccountVC.swift
 //  smack
 //
 //  Created by Alex LaDue on 11/28/17.
@@ -8,19 +8,16 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class CreateAccountVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
     }
-    
-    
+
     @IBAction func closePressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
-    @IBAction func createAccountBtn(_ sender: Any) {
-        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
-    }
 }
