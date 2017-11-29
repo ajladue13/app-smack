fd7b90b447e310885d04ceaea276cf3454651ff6
Alex LaDue <aj.ladue@gmail.com>
2017-11-28 20:44:52 -0500

    Created LoginVC file and storyboard

---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Alex LaDue on 11/28/17.
+//  Copyright © 2017 Alex LaDue. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
