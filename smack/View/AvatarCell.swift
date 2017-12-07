3c50f52a49ee779d830f902a41b3bb44c9ed3602
Alex LaDue <aj.ladue@gmail.com>
2017-12-06 23:43:39 -0500

    Create the Avatar Picker UI and VC

---
+++ smack/View/AvatarCell.swift
@@ -0,0 +1,27 @@
+//
+//  AvatarCell.swift
+//  smack
+//
+//  Created by Alex LaDue on 12/6/17.
+//  Copyright © 2017 Alex LaDue. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView(){
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+    
+}
