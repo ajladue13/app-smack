135361f379a757adf42bbaa553a62ce260d40b67
Alex LaDue <aj.ladue@gmail.com>
2017-12-19 21:17:58 -0500

    Create the ProfileVC and its XIB file. Now will open the VC when you click on the profile name when you are currently logged in

--- smack/Services/UserDataService.swift
+++ smack/Services/UserDataService.swift
@@ -64,4 +64,16 @@ class UserDataService{
         
     }
     
+    func logoutUser() {
+        id  = ""
+        avatarColor = ""
+        avatarName = ""
+        email = ""
+        name = ""
+        AuthService.instance.isLoggedIn = false
+        AuthService.instance.userEmail = ""
+        AuthService.instance.authToken = ""
+    }
+    
+    
 }
