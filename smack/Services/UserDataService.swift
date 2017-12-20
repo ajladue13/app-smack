8c191216647ebeb4e7ec1d01226cfd0c6d22b52a
Alex LaDue <aj.ladue@gmail.com>
2017-12-19 20:41:05 -0500

    Use notifications, observe notification. Use as scanner to scan text. Grab UI Color from database with new account and display that bg color in the UI

--- smack/Services/UserDataService.swift
+++ smack/Services/UserDataService.swift
@@ -30,4 +30,38 @@ class UserDataService{
         self.avatarName = avatarName
     }
     
+    //returns the color string saved in the database into a new UIColor that can be used
+    func returnUIColor(components: String) -> UIColor {
+        
+        let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a: NSString?
+        
+        // Need the & to work
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        
+        guard let rUnwrapped = r else {return defaultColor}
+        guard let gUnwrapped = g else {return defaultColor}
+        guard let bUnwrapped = b else {return defaultColor}
+        guard let aUnwrapped = a else {return defaultColor}
+        
+        let rfloat = CGFloat(rUnwrapped.doubleValue)
+        let gfloat = CGFloat(gUnwrapped.doubleValue)
+        let bfloat = CGFloat(bUnwrapped.doubleValue)
+        let afloat = CGFloat(aUnwrapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rfloat, green: gfloat, blue: bfloat, alpha: afloat)
+        
+        return newUIColor
+        
+    }
+    
 }
